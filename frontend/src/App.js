40445f8faada95eb5d248510474add88e0430baa
rahul122703 <killbusyness@gmail.com>
2025-01-30 10:30:16 +0530

    adding fr

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchForm from "./SearchForm";
+import Stories from "./Stories";
+import Buttons from "./Buttons";
+function App() {
+  return <div className="flex flex-col border border-black">hey this is code sprint project</div>;
+}
+
+export default App;
